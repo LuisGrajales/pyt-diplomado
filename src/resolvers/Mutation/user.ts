7c546af9279fa69827d22499214ec7ddae40ec7d
LuisGrajales <luis.logs1@gmail.com>
2021-09-14 21:08:53 -0500

    feat: resolvers added

--- src/resolvers/Mutation/user.ts
+++ src/resolvers/Mutation/user.ts
@@ -1,11 +1,21 @@
 import * as bcrypt from "bcryptjs";
+import * as randomstring from "randomstring";
+import { UserCreateInput } from "../../generated/prisma-client";
 import { Context } from "../../utils";
 
 export default {
   async setPwd(parent, args, ctx: Context) {
     const { pwd, token } = args.data;
     const password = await bcrypt.hash(pwd, 10);
-    return await ctx.prisma.updateUser({ data: { password }, where: { token_verify: token}});
+    return await ctx.prisma.updateUser({ data: { password }, where: { token }});
   },
-  updateUser: (parent, args, ctx: Context) => ctx.prisma.updateUser(args),
+  updateUser: (parent, args: { data: any, where: any }, ctx: Context) => ctx.prisma.updateUser(args),
+  createUser: (parent, { data }: { data: UserCreateInput}, ctx: Context) => {
+    const token = randomstring.generate(8).toUpperCase();
+    data = {
+      token,
+      ...data
+    };
+    return ctx.prisma.createUser(data);
+  },
 };
