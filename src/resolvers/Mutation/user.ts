230e23d3fff7401a8dd1866d6162dc0b16a0efb9
LuisGrajales <luis.logs1@gmail.com>
2021-09-20 21:39:34 -0500

    refactor: create users and login users

--- src/resolvers/Mutation/user.ts
+++ src/resolvers/Mutation/user.ts
@@ -3,20 +3,24 @@ import * as randomstring from "randomstring";
 import { UserCreateInput } from "../../generated/prisma-client";
 import { Context } from "../../utils";
 
+
+async function setPwd( pwd ) {
+  const password = await bcrypt.hash(pwd, 10);
+  return password
+}
+
 export default {
-  async setPwd(parent, args, ctx: Context) {
-    const { pwd, token } = args.data;
-    const password = await bcrypt.hash(pwd, 10);
-    return await ctx.prisma.updateUser({ data: { password }, where: { token }});
-  },
-  updateUser: (parent, args: { data: any, where: any }, ctx: Context) => ctx.prisma.updateUser(args.data),
+  
+  updateUser: (parent, args: { data: any, where: any }, ctx: Context) => ctx.prisma.updateUser(args),
   createUser: (parent, { data }: { data: UserCreateInput}, ctx: Context) => {
     const token = randomstring.generate(8).toUpperCase();
+    const salt = bcrypt.genSaltSync(10)
+    const password = bcrypt.hashSync(data.password, salt)
     data = {
       token,
-      ...data
+      ...data,
+      password
     };
-    return ctx.prisma.createUser(data);
+    return ctx.prisma.createUser(data)
   },
-  deleteUser: (parent, args: { data: any, where: any }, ctx: Context) => ctx.prisma.deleteUser(args.data),
 };
